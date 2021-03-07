4044e29eac6e0df6010cc4682e7cef6ade5ebadd
Henry Barreto <contato@henrybarreto.dev>
2021-03-07 01:40:54 -0300

    Harding food consumition

--- src/dev/henrybarreto/hardcoreplus/HardcorePlusEvents.java
+++ src/dev/henrybarreto/hardcoreplus/HardcorePlusEvents.java
@@ -9,14 +9,16 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
 public class HardcorePlusEvents implements Listener {
-    public static final double MULTIPLIER_MOB_DAMAGE = 4.5;
+    public static final double MULTIPLIER_MOB_DAMAGE = 3;
     public static final double MULTIPLIER_MOB_DMG_TAKEN = 2.5;
     public static final int MULTIPLIER_XP_GAIN = 2;
+    public static final int MULTIPLIER_FOOD_GAIN = 2;
 
     @EventHandler
     public void EntityDamageEvent(EntityDamageEvent event) {
@@ -46,6 +48,12 @@ public class HardcorePlusEvents implements Listener {
             zombieChain.handleMob(mob);
         }
     }
+
+    @EventHandler
+    public void PlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
+        event.getPlayer().setFoodLevel(event.getPlayer().getFoodLevel() - MULTIPLIER_FOOD_GAIN);
+    }
+
     @EventHandler
     public void PlayerExpChangeEvent(PlayerExpChangeEvent event) {
         event.setAmount(
